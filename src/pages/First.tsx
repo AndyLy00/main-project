1c3e1d057cb20fe4bc41c97ae8f4745a5a6521dd
AndyLy00 <andreia.cristina.siretanu@gmail.com>
2022-12-29 09:31:26 +0200

    ANTD project v1

--- src/pages/First.tsx
+++ src/pages/First.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Layout} from "antd";
+import {Button, Layout} from "antd";
 import cats from "../images/cats.jpg";
 
 export const First = () => {
@@ -10,6 +10,10 @@ export const First = () => {
             <Content style={{margin: '24px 16px 0', overflow: 'initial'}}>
                 <div style={{padding: 24, textAlign: 'center'}} className="page_background">
                     <img alt="/" src={cats} className="image"/>
+                    <div>
+                        <Button className="button">Cute</Button>
+                        <Button className="button">Adorable</Button>
+                    </div>
                 </div>
             </Content>
         </>
